db50c939b24d554aeca5f86dbf331a0d761abffe
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-17 10:19:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DivideByZeroException
+{
+    public class DivByzero
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("enter a and b val");
+                try
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+                    int val = a / b;
+                    Console.WriteLine(val);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
